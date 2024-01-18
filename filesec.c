d204dbb7985bd0673776e328ad149fce228fe27b
krpeterson36 <129975080+krpeterson36@users.noreply.github.com>
2024-01-18 09:22:34 -0800

    Add files via upload

---
+++ filesec.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+
+    char output_file_name[128];    //You may assume that the length of the output file name will not exceed 128 characters.
+    
+    return 0;
+
+}
