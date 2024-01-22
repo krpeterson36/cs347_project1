51704d26fa65c76cca7088edd327aff3dc7f4a58
Kyle Peterson <kylepeterson@kyles-air-273.wlan.wwu.edu>
2024-01-22 09:40:28 -0800

    Added comments and renamed some methods for mac compatibility

--- filesec.c
+++ filesec.c
@@ -7,13 +7,18 @@
 
 #define ERROR "./filesec -e|-d [filename.txt]\n"
 
-int encrypt(char* encName, char* dest){
+//encrypts the file, returns -1 on failure, 0 on success
+int fileEncrypt(char* encName, char* dest){
+
+    //opens the file that we will encrypt
     int encFile = open(encName, O_RDONLY);
     if(encFile == -1){
         printf("%s", ERROR);
         close(encFile);
         return -1;
     }
+
+    //opens the target file
     int encDest = open(dest, O_WRONLY|O_CREAT|O_TRUNC, 00700);
     if(encDest == -1){
         printf("%s", ERROR);
@@ -21,6 +26,8 @@ int encrypt(char* encName, char* dest){
         close(encDest);
         return -1;
     }
+
+    //Read's one bit at a time into unEnc, then adds 100 to it and saves it to enc, then writes 1 bit from enc to destFile
     int unEnc[1];
     int enc[1];
     while(read(encFile, unEnc, 1) > 0){ 
@@ -32,17 +39,27 @@ int encrypt(char* encName, char* dest){
     return 0;
 }
 
-int decrypt(char* decName, char* dest){
+//decrypts the file, returns -1 on failure, 0 on success
+int fileDecrypt(char* decName, char* dest){
+
+    //opens source file
     int decFile = open(decName, O_RDONLY);
     if(decFile == -1){
         printf("%s", ERROR);
+        close(decFile);
         return -1;
     }
+
+    //opens target file
     int decDest = open(dest, O_WRONLY|O_CREAT|O_TRUNC, 00700);
     if(decDest == -1){
         printf("%s", ERROR);
+        close(decFile);
+        close(decDest);
         return -1;
     }
+
+    //reads 1 bit from source file to unDec, decrements unDec by 100 and stores in dec, reads 1 bit from dec to destFile
     int unDec[1];
     int dec[1];
     while(read(decFile, unDec, 1) > 0){
@@ -58,37 +75,44 @@ int main(int argc, char** argv)
 {
 
     char output_file_name[128];    //You may assume that the length of the output file name will not exceed 128 characters.
+
+    //Checks to see that there are exactly 3 arguments
     if(argc != 3){
         printf("%s", ERROR);
         return -1;
     }
+
+    //gets the file name
     int i = 0;
     while(argv[2][i] != '.'){
         output_file_name[i] = argv[2][i];
         i++;
     }
+
+    //sets the rest of the array to null
     while(i < 128){
         output_file_name[i] = '\0';
         i++;
     }
+
+    //Determins which flag the program was run with
     int opt;
     while((opt = getopt(argc, argv, "e:d:")) != -1){
         switch (opt){
             case 'e':
                 strcat(output_file_name, "_enc.txt");
-                printf("%s\n", output_file_name);
-                return encrypt(argv[2], output_file_name);
+                return fileEncrypt(argv[2], output_file_name);
                 break;
             case 'd':
                 strcat(output_file_name, "_dec.txt");
-                printf("%s\n", output_file_name);
-                return decrypt(argv[2], output_file_name);
+                return fileDecrypt(argv[2], output_file_name);
                 break;
             case '?':
                 printf("%s", ERROR);
                 return -1;
         }
     }
+
     return 0;
 
 }
